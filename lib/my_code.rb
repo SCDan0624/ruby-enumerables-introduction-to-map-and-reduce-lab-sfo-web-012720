0785463cfeed1fe786c0b36a35aa93abb069510b
Daniel Mayle <dmayle012@gmail.com>
2019-12-24 22:49:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,52 @@
-# My Code here....
+
+def map_to_negativize(array)
+  new_arr = []
+  i = 0
+  
+  while i < array.length do 
+    new_arr << array[i] * -1
+    i += 1
+  end 
+  
+  new_arr
+end
+
+def map_to_no_change(array)
+  array
+end
+
+def map_to_double(array)
+  new_arr = []
+  i = 0 
+  
+  while i < array.length do 
+    new_arr << array[i] * 2
+    i += 1 
+  end 
+  
+  new_arr
+end
+
+def map_to_square(array)
+  new_arr = []
+  i = 0 
+  
+  while i < array.length do 
+    new_arr.push(array[i] * array[i])
+    i += 1 
+  end
+  
+  new_arr
+  
+end
+
+def reduce_to_total(array)
+  i = 0 
+  total = 0
+  
+  while i < array.length do 
+    total += array[i]
+  end
+  
+  total 
+end 
